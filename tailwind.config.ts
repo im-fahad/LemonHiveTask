cef0e41ff27a6bddc9e7c12fd305b1825f760c5a
im-fahad <abdullahalfahad.bd@gmail.com>
2024-03-13 23:16:36 +0600

    working on hero asection

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,20 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily:{
+        Inter: ['Inter', 'sans-serif']
+      },
+      colors:{
+        primary:'#111D5E',
+        secondary:'#FFC93E',
+        blue:'#2563EB',
+        grayDark:'#617187',
+        gray:'#CDCDCD',
+        grayLight:'#F9FAFB'
+      },
+      container: {
+        center: true,
+      },
     },
   },
   plugins: [],
